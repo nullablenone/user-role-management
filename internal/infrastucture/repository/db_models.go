ab107135a199ecaffd6a1b7ff14d183bc45353a2
useryesa9@gmail.com <useryesa9@gmail.com>
2025-12-15 11:05:06 +0700

    fix: remove gorm.Model to resolve missing deleted_at column error

--- internal/infrastucture/repository/db_models.go
+++ internal/infrastucture/repository/db_models.go
@@ -1,12 +1,16 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"time"
+)
 
 // RoleModel adalah representasi tabel 'roles' untuk GORM.
 type RoleModel struct {
-	gorm.Model
+	ID          uint   `gorm:"primarykey"`
 	Name        string `gorm:"type:varchar(50);uniqueIndex;not null"`
 	Deskription string `gorm:"type:varchar(50);not null"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
 
 func (RoleModel) TableName() string {
@@ -15,12 +19,14 @@ func (RoleModel) TableName() string {
 
 // UserModel adalah representasi tabel 'users' untuk GORM.
 type UserModel struct {
-	gorm.Model
-	Name     string    `gorm:"type:varchar(50);not null"`
-	Email    string    `gorm:"type:varchar(350);uniqueIndex;not null"`
-	Password string    `gorm:"type:varchar(350);not null"`
-	RoleID   uint      `gorm:"default:1"`
-	Role     RoleModel `gorm:"foreignKey:RoleID"`
+	ID        uint      `gorm:"primarykey"`
+	Name      string    `gorm:"type:varchar(50);not null"`
+	Email     string    `gorm:"type:varchar(350);uniqueIndex;not null"`
+	Password  string    `gorm:"type:varchar(350);not null"`
+	RoleID    uint      `gorm:"default:1"`
+	Role      RoleModel `gorm:"foreignKey:RoleID"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 func (UserModel) TableName() string {
